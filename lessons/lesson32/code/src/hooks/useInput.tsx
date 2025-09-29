e3ca927359b8f741411f0a27129cac1c440c5131
Осипук <andrey.o@list.ru>
2025-09-29 18:49:23 +0300

    Add lesson 32

---
+++ lessons/lesson32/code/src/hooks/useInput.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+/**
+ * @param {string} initialValue - Начальное значение поля.
+ * @returns {{value: string, onChange: function}} - Объект со значением и обработчиком.
+ */
+export function useInput(initialValue = "") {
+  // TODO: Реализуйте хук.
+  // 1. Создайте состояние для хранения значения поля с помощью useState.
+  // 2. Создайте функцию-обработчик onChange, которая будет обновлять это состояние, получая значение из event.target.value.
+  // 3. Верните объект, содержащий текущее значение (value) и обработчик (onChange).
+}
